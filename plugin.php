dbce797f3587016ee91c3821828437c1318e3da5
Frank Barrera <frankb2320@gmail.com>
2016-02-11 13:18:25 -0430

    iniciando

---
+++ plugin.php
@@ -0,0 +1,15 @@
+<?php
+/*
+plugin name: FarkbarnPlugWP
+plugin URI: http://127.0.0.1
+Description: Plugin para agregar funciones especiales
+Author: Frank Barrera - @farkbarn
+Author URI:http://www.bcpublicidad.com
+Version: 0.0.0
+License: GNU General Public License V3
+License URI: http://www.gnu.org/copyleft/gpl.html
+
+
+*/
+
+?>
